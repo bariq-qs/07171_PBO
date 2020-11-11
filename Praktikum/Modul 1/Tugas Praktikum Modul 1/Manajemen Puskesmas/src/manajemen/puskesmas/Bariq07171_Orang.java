68368309a24bb4f07fa7256f374a02657b024c97
bariq-qs <bariq.xtkj1.14@gmail.com>
2020-11-11 19:20:03 +0700

    tugas praktikum modul 1 setelah acc asisten

---
+++ Praktikum/Modul 1/Tugas Praktikum Modul 1/Manajemen Puskesmas/src/manajemen/puskesmas/Bariq07171_Orang.java
@@ -0,0 +1,32 @@
+
+package manajemen.puskesmas;
+
+public class Bariq07171_Orang {
+    String Bariq07171_Nama, Bariq07171_Alamat, Bariq07171_Username, Bariq07171_Password, Bariq07171_JenisKelamin;
+    int Bariq07171_NIK;
+    public Bariq07171_Orang(String Bariq07171_Nama, String Bariq07171_Alamat, String Bariq07171_Password, String Bariq07171_JenisKelamin, int Bariq07171_NIK) {
+        this.Bariq07171_Nama = Bariq07171_Nama;
+        this.Bariq07171_Alamat = Bariq07171_Alamat;
+        this.Bariq07171_Password = Bariq07171_Password;
+        this.Bariq07171_JenisKelamin = Bariq07171_JenisKelamin;
+        this.Bariq07171_NIK = Bariq07171_NIK;
+    }
+    public String getBariq07171_Username() {
+        return Bariq07171_Username;
+    }
+    public String getBariq07171_Password() {
+        return Bariq07171_Password;
+    }
+    public String getBariq07171_Nama() {
+        return Bariq07171_Nama;
+    }
+    public String getBariq07171_Alamat() {
+        return Bariq07171_Alamat;
+    }
+    public String getBariq07171_JenisKelamin() {
+        return Bariq07171_JenisKelamin;
+    }
+    public int getBariq07171_NIK() {
+        return Bariq07171_NIK;
+    }
+}
