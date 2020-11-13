04006088e8f6e3999c5cd9830bf65e9bb65dfe52
bariq-qs <bariq.xtkj1.14@gmail.com>
2020-11-13 07:42:50 +0700

    penambahan tugas asistensi dosen:
    1. Menghilangkan function static pada modul 1

---
+++ Praktikum/Modul 1/Tugas Resmi Modul 1/Manajemen Puskesmas/src/manajemen/puskesmas/Bariq07171_Poli.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package manajemen.puskesmas;
+
+/**
+ *
+ * @author Bariq Qushoyyi
+ */
+public class Bariq07171_Poli {
+    static String [] Poli = {"GIGI", "UMUM", "ANAK"};
+}
---
+++ Praktikum/Modul 1/Tugas Resmi Modul 1/Manajemen Puskesmas/src/manajemen/puskesmas/ManajemenPuskesmas.java
@@ -0,0 +1,118 @@
+package manajemen.puskesmas;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ManajemenPuskesmas {
+    public ArrayList<Bariq07171_Orang> dataOrang = new ArrayList();
+    public Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        ManajemenPuskesmas m = new ManajemenPuskesmas();
+        m.menu();
+    }
+
+    public void menu(){
+        System.out.println("Selamat Datang Di Puskesmas ABC !!!");
+        int pil;
+        do {
+            System.out.println("1. Daftar Akun Baru");
+            System.out.println("2. List Akun Terdaftar");
+            System.out.println("3. Login");
+            System.out.println("0. Exit");
+            System.out.print("Pilih = ");
+            pil = input.nextInt();
+            switch (pil) {
+                case 1:
+                    daftar();
+                    break;
+                case 2:
+                    listView();
+                    break;
+                case 3:
+                    System.out.print("NIK = ");
+                    int nik = input.nextInt();
+                    System.out.print("Password = ");
+                    String pass = input.next();
+                    Login(nik, pass);
+                    break;
+            }
+        } while (pil != 0);    
+    }
+    
+    public void daftar() {
+        System.out.print("Input Nama = ");
+        String nama = input.next();
+        System.out.print("Input NIK = ");
+        int nik = input.nextInt();
+        System.out.print("Input Alamat = ");
+        String alamat = input.next();
+        System.out.print("Input Jenis Kelamin = ");
+        String jeniskelamin = input.next();
+        System.out.print("Input Password = ");
+        String password = input.next();
+        dataOrang.add(new Bariq07171_Orang(nama, alamat, password, jeniskelamin, nik));
+    }
+
+    public void Login(int nik, String password) {
+        for (int i = 0; i < dataOrang.size(); i++) {
+            if (nik == dataOrang.get(i).getBariq07171_NIK() 
+                    && password.equals(dataOrang.get(i).getBariq07171_Password())) {
+                System.out.println("Nama = " + dataOrang.get(i).getBariq07171_Nama());
+                System.out.println("NIK = " + dataOrang.get(i).getBariq07171_NIK());
+                System.out.println("Alamat = " + dataOrang.get(i).getBariq07171_Alamat());
+                System.out.println("Jenis Kelamin = " + dataOrang.get(i).getBariq07171_NIK());
+                int pilihan;
+                do {
+                    System.out.println("1. Edit Akun");
+                    System.out.println("2. Hapus Akun");
+                    System.out.println("3. Logout");
+                    System.out.print("Pilih = ");
+                    pilihan = input.nextInt();
+                    switch (pilihan) {
+                        case 1:
+                            EditAkun(nik, i);
+                            break;
+                        case 2: 
+                            DeleteAkun(i);
+                            System.out.println("Data Berhasil Dihapus");
+                        case 3:
+                            System.out.println("Terima Kasih");
+                    }
+                } while (pilihan != 2 && pilihan !=3);
+            }
+        }
+    }
+    
+    public void listView(){
+        for (int i = 0; i < dataOrang.size(); i++) {
+            if (dataOrang.size() > 0){
+            System.out.println("<================================================>");
+            System.out.println("Nama = " + dataOrang.get(i).getBariq07171_Nama());
+            System.out.println("NIK = " + dataOrang.get(i).getBariq07171_NIK());
+            System.out.println("Alamat = " + dataOrang.get(i).getBariq07171_Alamat());
+            System.out.println("Jenis Kelamin = " + dataOrang.get(i).getBariq07171_NIK());
+            System.out.println("<================================================>");
+            }else{
+                System.out.println("Data Kosong");
+            }
+        }
+    }
+
+    public void EditAkun(int nik, int index) {
+        System.out.print("Input Nama = ");
+        String nama = input.next();
+        System.out.print("Input Alamat = ");
+        String alamat = input.next();
+        System.out.print("Input Jenis Kelamin = ");
+        String jeniskelamin = input.next();
+        System.out.print("Input Password = ");
+        String password = input.next();
+        dataOrang.set(index, new Bariq07171_Orang(nama, alamat, password, jeniskelamin, nik));
+    }
+    
+    public void DeleteAkun (int index){
+        dataOrang.remove(index);
+    }
+
+}
